0214aabdd09089b7761ece3431345a11a425979f
REDE\m18633 <luiz.chiaradia@almg.gov.br>
2022-06-02 13:09:56 -0300

    update

--- js/custom/bigger-picture.js
+++ js/custom/bigger-picture.js
@@ -1,4 +1,5 @@
 import BiggerPicture from "bigger-picture";
+import Macy from "macy";
 
 // initialize BiggerPicture
 const bp = BiggerPicture({
@@ -7,18 +8,34 @@ const bp = BiggerPicture({
 
 // grab image links
 
-const imageLinks = document.querySelectorAll(".images > a");
+const imageLinks = document.querySelectorAll(".galleyimages > a");
 
-// add click listener to open BiggerPicture
-for (let link of imageLinks) {
-  link.addEventListener("click", openGallery);
-}
+if (imageLinks.length != 0) {
+  // add click listener to open BiggerPicture
+  for (let link of imageLinks) {
+    link.addEventListener("click", openGallery);
+  }
 
-// open BiggerPicture
-function openGallery(e) {
-  e.preventDefault();
-  bp.open({
-    items: imageLinks,
-    el: e.currentTarget
+  // open BiggerPicture
+  function openGallery(e) {
+    e.preventDefault();
+    bp.open({
+      items: imageLinks,
+      el: e.currentTarget
+    });
+  }
+
+
+  // masonry grid
+  var mygallery = Macy({
+    container: ".galleyimages",
+    trueOrder: true,
+    margin: 4,
+    columns: 3,
+    breakAt: {
+      520: {
+        columns: 2
+      }
+    }
   });
 }
